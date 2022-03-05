6c1ad6e6f8a45a096ff3054169f018c940d2d61b
Satyam Bhawsinghka <bhawsins@oregonstate.edu>
2022-03-05 15:12:13 -0800

    Adding all files

---
+++ output.py
@@ -0,0 +1,60 @@
+import numpy
+import os
+
+class Output():
+    def __init__(self, mode=None, x=None, y=None, filename='default'):
+        self.save_dir = os.path.dirname(os.path.realpath(__file__)) + "/output/"
+        self.file_name = filename
+        if x is not None:
+            self.x = x
+        else:
+            print("Input deformations in X")
+        if y is not None:
+            self.y = y
+        else:
+            print("Input deformations in Y")
+
+        if mode == 'image':
+            self.output_image()
+        elif mode == 'video':
+            self.output_video()
+        else:
+            print("Enter mode either video or image")
+
+    def output_video(self):
+        with open(self.save_dir + f"def_x_{self.file_name}.csv", "w") as f:
+            f.write(f"Deformation for frame {0}")
+            f.write("\n")
+            numpy.savetxt(f, self.x[0], delimiter=',', fmt='%.2f')
+            f.write("\n")
+
+        with open(self.save_dir + f"def_y_{self.file_name}.csv", "w") as f:
+            f.write(f"Deformation for frame {0}")
+            f.write("\n")
+            numpy.savetxt(f, self.y[0], delimiter=',', fmt='%.2f')
+            f.write("\n")
+
+        for i in range(1,len(self.x)):
+            with open(self.save_dir + f"def_x_{self.file_name}.csv", "a") as f:
+                f.write(f"Deformation for frame {i}")
+                f.write("\n")
+                numpy.savetxt(f, self.x[i], delimiter=',', fmt='%.2f')
+                f.write("\n")
+
+        for i in range(1,len(self.y)):
+            with open(self.save_dir + f"def_y_{self.file_name}.csv", "a") as f:
+                f.write(f"Deformation for frame {i}")
+                f.write("\n")
+                numpy.savetxt(f, self.y[i], delimiter=',', fmt='%.2f')
+                f.write("\n")
+
+
+
+    def output_image(self):
+        with open(self.save_dir + f"def_x_{self.file_name}.csv", "w") as f:
+            numpy.savetxt(f, self.x, delimiter=',', fmt='%.2f')
+        with open(self.save_dir + f"def_y_{self.file_name}.csv", "w") as f:
+            numpy.savetxt(f, self.y, delimiter=',', fmt='%.2f')
+
+
+
---
+++ visualization.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from PIL import Image
+import os
+
+def vis_plotter(x, y, img):
+
+
+    ig, ax = plt.subplots()
+
+
+    plt.subplot(1, 3, 1)
+    plt.title("Deformation Image")
+    imgplot = plt.imshow(img)
+
+
+    plt.subplot(1, 3, 2)
+    plt.title("X Deformations")
+    imgplot = plt.imshow(x)
+    imgplot.set_cmap('YlGnBu')
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    plt.colorbar()
+
+    plt.subplot(1, 3, 3)
+    plt.title("Y Deformations")
+    imgplot = plt.imshow(y)
+    imgplot.set_cmap('YlGnBu')
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    plt.colorbar()
+
+    plt.show()
+    time.sleep(0.5)
+    plt.close(ig)
+    return
+
+class Visualization():
+    def __init__(self, mode=None, def_video=None, def_image=None, x=None, y=None):
+        if x is not None:
+            self.x = x
+        else:
+            print("Input deformations in X")
+        if y is not None:
+            self.y = y
+        else:
+            print("Input deformations in Y")
+
+        if mode == 'image':
+            if def_image is not None:
+                self.image = def_image
+            else:
+                print("Mode selected is image. Input deformed image")
+            self.show_image()
+        elif mode == 'video':
+            if def_video is not None:
+                self.video = def_video
+            else:
+                print("Mode selected is image. Input deformation video")
+            self.show_video()
+        else:
+            print("Enter mode either video or image")
+
+    def show_video(self):
+        image = Image.open(self.video)
+        # Display individual frames from the loaded animated GIF file
+        for frame in range(image.n_frames):
+            image.seek(frame)
+            img = image.copy()
+            vis_plotter(self.x[frame], self.y[frame], img)
+        image.close()
+
+    def show_image(self):
+        image = Image.open(self.image)
+        vis_plotter(self.x, self.y, image)
+        image.close()
+
+
+
+if __name__ == '__main__':
+    Visualization(mode='video', def_video='generated/def_translate_x.gif', x='output/sample_x.npy', y='sample_y.npy')
