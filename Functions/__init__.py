355c5923f7153ba368fb5d1e7be2ec20fb2a366f
Satyam Bhawsinghka <bhawsins@oregonstate.edu>
2022-03-15 13:15:46 -0700

    debug

--- Functions/__init__.py
+++ Functions/__init__.py
@@ -1,2 +1,2 @@
-from creating_deformation import main as create
-from measuring_deformation import main as measure
+from deformeasure.Functions.creating_deformation import main as create
+from deformeasure.Functions.measuring_deformation import main as measure
